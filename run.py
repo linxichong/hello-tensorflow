d463fe6e981b9c0cb1e6d723dc021f5573cdbbfc
linxichong <gogailinxichong@gmail.com>
2019-09-12 23:41:08 +0900

    modify

---
+++ run.py
@@ -0,0 +1,11 @@
+from imageio import imread
+
+img = imread('assets/pic.jpg')
+print(img.dtype, img.shape)
+print(img)
+# v = np.array([1,2,3])
+# w = np.array([4,5])
+
+# print(v)
+# print(np.reshape(v, (3,1)))
+# print(w)
